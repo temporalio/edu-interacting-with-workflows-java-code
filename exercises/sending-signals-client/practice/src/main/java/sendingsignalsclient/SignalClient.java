c515a86ef857c5845a2bc00c479450ebf0d5be83
Mason Egger <mason@masonegger.com>
2024-04-07 09:44:50 -0500

    adding the new exercise 2

---
+++ exercises/sending-signals-client/practice/src/main/java/sendingsignalsclient/SignalClient.java
@@ -0,0 +1,22 @@
+package sendingsignalsclient;
+
+import io.temporal.client.WorkflowClient;
+import io.temporal.serviceclient.WorkflowServiceStubs;
+import sendingsignalsclient.orderpizza.PizzaWorkflow;
+
+
+public class SignalClient {
+  public static void main(String[] args) throws Exception {
+
+    WorkflowServiceStubs service = WorkflowServiceStubs.newLocalServiceStubs();
+
+    WorkflowClient client = WorkflowClient.newInstance(service);
+
+    // TODO: PART A: Set Workflow ID
+    PizzaWorkflow workflow = client.newWorkflowStub(PizzaWorkflow.class, "CHANGE_ME");
+
+    // TODO: PART A: Call fulfillOrderSignal, passing `true`
+
+    System.exit(0);
+  }
+}
---
+++ exercises/sending-signals-client/practice/src/main/java/sendingsignalsclient/exceptions/InvalidChargeAmountException.java
@@ -0,0 +1,20 @@
+package sendingsignalsclient.exceptions;
+
+public class InvalidChargeAmountException extends Exception {
+
+  public InvalidChargeAmountException() {
+    super();
+  }
+
+  public InvalidChargeAmountException(String message) {
+    super(message);
+  }
+
+  public InvalidChargeAmountException(String message, Throwable cause) {
+    super(message, cause);
+  }
+
+  public InvalidChargeAmountException(Throwable cause) {
+    super(cause);
+  }
+}
